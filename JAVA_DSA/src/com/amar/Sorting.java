5b4b48bf18a7614acc6113001b31d44e7cccc424
AMARNATH V <95960348+Amarnath02@users.noreply.github.com>
2023-08-16 12:15:39 +0530

    Sorting.java

--- JAVA_DSA/src/com/amar/Sorting.java
+++ JAVA_DSA/src/com/amar/Sorting.java
@@ -3,9 +3,15 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr = {29, 22, 33, 12, 21};
+        int[] arr1 = {5, 4, 3, 2, 1};
         Insertion(arr);
+        Cyclic(arr1);
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
     }
+    
+    // Bubble Sort
+    
     static void Bubble(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length - i; j++) {
@@ -15,6 +21,9 @@ public class Sorting {
             }
         }
     }
+    
+    // Selection Sort
+    
     static void Selection(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             int last = arr.length - i - 1;
@@ -22,6 +31,9 @@ public class Sorting {
             swap(arr, max, last);
         }
     }
+    
+    // Insertion Sort
+    
     static void Insertion(int[] arr){
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i+1; j > 0; j--) {
@@ -34,6 +46,22 @@ public class Sorting {
             }
         }
     }
+    
+    // Cyclic Sort
+    
+    static void Cyclic(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+    
     static int getMax(int[] arr, int start, int last){
         int max = start;
         for (int i = start; i <= last; i++) {
@@ -43,6 +71,7 @@ public class Sorting {
         }
         return max;
     }
+    
     static void swap(int[] arr, int first, int second){
         int temp = arr[first];
         arr[first] = arr[second];
